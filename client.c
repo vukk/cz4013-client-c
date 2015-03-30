2aae0807ee90d5c8324536ec7a57396390f0e3d7
Unto Kuuranne <unto.kuuranne@aalto.fi>
2015-03-30 16:36:15 +0800

    Adds socket binding and remote address construction.

--- client.c
+++ client.c
@@ -46,19 +46,23 @@ void cmd_monitor(char *id, char *time);
 
 
 int main(int argc, char **argv) {
+	// addresses
 	struct sockaddr_in addr_self, addr_remote;
-	int socket_fd;
-	char inputbuffer[1000];
-	char **myargv;
-	int myargc;
+	memset((char *)&addr_self, 0, sizeof(addr_self));
+	memset((char *)&addr_remote, 0, sizeof(addr_remote));
+
+	int socket_fd; // socket file descriptor
+	char inputbuffer[1000]; // buffer for reading user input
+	char **myargv; // the command inputted by user as argument vector
+	int myargc;    // argument count for the previous vector
 
 	if (argc < 3) {
 		printf("Usage: client <server ip> <server port>\n");
 		return 1;
 	}
 
-	char* server = argv[1];
-	int port;
+	char* server = argv[1]; // server ip
+	int port;				// server port
 	if (!sscanf(argv[2], "%i", &port)) {
 		perror("ERROR: Server port has to be an integer.\n");
 		return 2;
@@ -67,22 +71,37 @@ int main(int argc, char **argv) {
 	printf("Welcome. Using server: %s port: %s\nConnecting...\n", argv[1], argv[2]);
 	// create socket
 	if ((socket_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-		perror("ERROR: can't create UDP socket.");
+		perror("ERROR: can't create UDP socket.\n");
 		return 3;
 	}
 
-	// TODO: bind upd socket, fill remote addr struct
+	// bind to every local ip address, pick any port
+	addr_self.sin_family = AF_INET;
+	addr_self.sin_addr.s_addr = htonl(INADDR_ANY);
+	addr_self.sin_port = htons(0);
+
+	if (bind(socket_fd, (struct sockaddr *)&addr_self, sizeof(addr_self)) < 0) {
+		perror("ERROR: binding local socket failed.\n");
+		return 4;
+	}
+
+	// fill remote address struct
+	addr_remote.sin_family = AF_INET;
+	addr_remote.sin_port = htons(port);
+	if (inet_aton(server, &remaddr.sin_addr)==0) {
+		perror("ERROR: converting server IP to binary address failed, please input proper IP.\n");
+		return 5;
+	}
+
 	// TODO: construct messages to send, send/receive messages
 	// TODO: add failures and tolerance
-
-
 
 	//// Command loop
 
 	puts(msg_input);
 
 	// exit with ^D, ^C, or typing "exit"
-	while(fgets(inputbuffer, sizeof inputbuffer, stdin)) {
+	while(fgets(inputbuffer, sizeof(inputbuffer), stdin)) {
 		// remove newline
 		inputbuffer[strcspn(inputbuffer, "\n")] = 0;
 		// parse arguments
